652851680ec38a6cc76991bb664453ef10a8ba7d
Mashen_Taterz <ty_beach@live.com>
2023-12-23 00:35:13 -0800

    TTT A2 Q# 2 & 3

--- src/TTT/A2.hs
+++ src/TTT/A2.hs
@@ -2,20 +2,25 @@ module TTT.A2 where
 
 import Data.List (intercalate)
 import TTT.A1
+import Data.Char (digitToInt)
 
 -- Q#01
 promptPlayer :: Player -> String 
 promptPlayer player = 
     concat ["Player ", show player, "'s turn: enter a row and column position."]
--- Q#02
 
-_RANGE_ = undefined
+-- Q#02
+_RANGE_ :: [Int]
+_RANGE_ = [0 .. 2]
 
 -- Q#03
+isDigit :: Char -> Bool
+isDigit digit = digit `elem` ['0' .. '9']
 
-isDigit = undefined
-
-readDigit = undefined
+readDigit :: Char -> Int
+readDigit digit
+    | isDigit digit = read [digit] :: Int 
+    | otherwise = -1
 
 -- Q#04
 
